e0cc4c816056e3c1f24c7e05bca96a60db46f565
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-05-31 19:51:23 +0300

    toggle added

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ arrows.forEach((arrow, i) => {
       }
     });
   });
+
+
+/* dark mode */
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container, .navbar, .sidebar, .sidebar i, .toggle, toggle-ball, .movie-list-filter select, .movie-list-title");
+
+ball.addEventListener("click", function() {
+    items.forEach((item) => item.classList.toggle("active"));
+});
