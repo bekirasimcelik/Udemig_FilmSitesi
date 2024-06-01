5da5c7f4f56ac2ce3a8f0b19a243d3128ca21f95
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-06-01 11:39:28 +0300

    responsive structure added

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow, i) => {
+    const widthRadio = Math.floor(window.innerWidth / 300);
     let clickCounter = 0;
     const imageItem = movieLists[i].querySelectorAll("img").length;
     arrow.addEventListener("click", function () {
       clickCounter++;
-      if (imageItem - (4 + clickCounter) >= 0) {
+      if (imageItem - (4 + clickCounter) + (4 - widthRadio) >= 0) {
         const computedStyle = window.getComputedStyle(movieLists[i]);
         const matrix = new WebKitCSSMatrix(computedStyle.transform);
         movieLists[i].style.transform = `translateX(${matrix.m41 - 300}px)`;
       } else {
         movieLists[i].style.transform = "translateX(0)";
+        clickCounter = 0;
       }
     });
   });
